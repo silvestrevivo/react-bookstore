a5758682227f6e8928ad1da5bca447b5b140bc22
silvestrevivo <silvestrevivomillan@gmail.com>
2018-02-07 08:35:24 +0100

    axios implementation

--- components/app.js
+++ components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Search from './search';
 import Gallery from './gallery';
+import axios from 'axios';
 
 class App extends Component {
     state = {
@@ -19,65 +20,13 @@ class App extends Component {
 
     search = (term) => {
         const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
-        fetch(`${BASE_URL}${term}`, { method: 'GET' })
-            .then(response => response.json())
-            .then(json => {
-                let { items } = json;
-                this.setState({ items });
+        axios.get(`${BASE_URL}${term}`)
+            .then(response => {
+                this.setState({
+                    items: response.data.items
+                });
             });
     }
 }
 
 export default App;
-
-
-
-
-
-
-
-
-// class App extends Component {
-//     state = {
-//         items: []
-//     };
-
-//     render() {
-//         return (
-//             <div className="container global">
-//                 <h1 className="text-center">BookStore</h1>
-//                 <FormGroup>
-//                     <InputGroup>
-//                         <FormControl
-//                             type="text"
-//                             //placeholder="Search for a book"
-//                             defaultValue="Harry Potter"
-//                             inputRef={input => (this.input = input)}
-//                             onKeyPress={event => {
-//                                 if (event.key === 'Enter') {
-//                                     this.search();
-//                                 }
-//                             }}
-//                         />
-//                         <InputGroup.Addon onClick={this.search}>
-//                             <Glyphicon glyph="search" />
-//                         </InputGroup.Addon>
-//                     </InputGroup>
-//                 </FormGroup>
-//                 <Gallery datafetch={this.state.items} />
-//             </div>
-//         );
-//     }
-
-//     search = () => {
-//         const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
-//         fetch(`${BASE_URL}${this.input.value}`, { method: 'GET' })
-//             .then(response => response.json())
-//             .then(json => {
-//                 let { items } = json;
-//                 this.setState({ items });
-//             });
-//     };
-// }
-
-// export default App;
