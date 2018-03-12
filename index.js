b05428d3e1191dbff22355460cb022dba6c88aee
silvestrevivo <silvestrevivomillan@gmail.com>
2018-03-12 17:08:01 +0100

    new set up based on starter

--- index.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app';
-import './style.scss';
-
-ReactDOM.render(<App />, document.getElementById('root'));
